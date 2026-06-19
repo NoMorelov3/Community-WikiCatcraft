8ffb1076e2f39b5fea2e50012bde085f53cffda3
EHqador <snaiper1331112@gmail.com>
2026-06-19 22:13:51 +0300

    ну жппжпжпжпж

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -8,51 +8,42 @@ export default defineConfig({
   description: "Данная вики была создана сообществом и не является оригинальной",
   lang: 'ru',
   publicDir: "Assets",
-  base: '/Community-WikiCatcraft/', // НАЗВАНИЕ ВАШЕГО ПРОЕКТА
+  base: '/Community-WikiCatcraft/', 
 
   head: [
-    // Правильный путь к логотипу вкладки (фавиконке) для вашего репозитория:
-    ['link', { rel: 'icon', href: 'icon_purple.png' }]
+    // ИСПРАВЛЕНО: Добавлен base и слэш, чтобы GitHub Pages нашел иконку во вкладке
+    ['link', { rel: 'icon', href: '/Community-WikiCatcraft/icon_purple.png' }]
   ],
 
-  
   themeConfig: {
+    // ДОБАВЛЕНО: Логотип в левом верхнем углу сайта
+    logo: '/icon_purple.png',
+
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Кланы', link: '/clans/all' },
-      // { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Кланы', link: '/clans/all' }
     ],
 
     sidebar: {
-      '/clans': [
-      {
-        text: 'Валькирия',
-        items: [
-          { text: 'История', link: '/clans/Valkyrie/History' },
-          { text: 'Общии фото', link: '/clans/Valkyrie/Photo' },
+      // ИСПРАВЛЕНО: Добавлен закрывающий слэш, чтобы сайдбар активировался внутри папки clans
+      '/clans/': [
+        {
+          text: 'Валькирия',
+          items: [
+            { text: 'История', link: '/clans/Valkyrie/History' },
+            { text: 'Общие фото', link: '/clans/Valkyrie/Photo' }
           ]
         }
       ],
-      '/': [
-
-      ],
-      /*
-      '/': [
-        
-      ],
-      '/': [
-        
-      ],
-      '/': [
-        
-      ],
-      */
-
+      
+      // Сайдбар для главной страницы (пока пустой)
+      '/': []
     },
 
     socialLinks: [
-      { icon: 'refinedgithub', link: 'https://github.com/NoMorelov3/Community-WikiCatcraft' },
-      { icon: 'spotify', link: 'https://open.spotify.com/playlist/05N92YWF1TCGfJMyH0gzHi?si=dfae0a5447b24a2a'}
+      // Изменено на стандартный github, так как refinedgithub может отображаться как пустой квадрат
+      { icon: 'github', link: 'https://github.com/NoMorelov3/Community-WikiCatcraft' },
+      { icon: 'spotify', link: 'https://open.spotify.com/playlist/05N92YWF1TCGfJMyH0gzHi?si=dfae0a5447b24a2a' }
     ]
   }
 })
