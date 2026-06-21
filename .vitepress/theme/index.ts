cb93c3939edc1c2649c1f35b38c21114d86ee20d
EHqador <snaiper1331112@gmail.com>
2026-06-21 11:19:55 +0300

    дай путь#2

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,20 +2,28 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import MusicPlayer from './MusicPlayer.vue'
+
+
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-top': () => h(MusicPlayer)
     })
+    
+    
   },
+  
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
+  
 } satisfies Theme
 
+  
 
 export interface Footer {
   // Сообщение, отображаемое прямо перед копирайтом.
