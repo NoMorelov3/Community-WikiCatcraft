0d81622ada4ee7c12804690cf0fa8979d1909cbe
EHqador <snaiper1331112@gmail.com>
2026-06-21 13:54:49 +0300

    тёмная тема

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 
 export default defineConfig({
+  appearance: false,
   srcDir: ".",
   title: "FanCC",
   description: "Данная вики была создана сообществом и не является оригинальной",
